57e31634b307a91782aae7f26727d0f0db888e12
prisar <pritimaysarkar2010@gmail.com>
2019-10-18 23:48:00 +0530

    submit button

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-// alert("hello");
+
+var myfunc = function(a) { // anonymous 
+    var list =    document.getElementById('addtodo')
+    var value1 = document.getElementById('invalues').value;
+    var listitem = document.createElement("li")
+       
+
+    var checkbox = document.createElement("input")
+    checkbox.type="checkbox";
+    checkbox.className="checkbox-margin";
+
+    listitem.appendChild(checkbox);
+    console.log(listitem);
+    console.log('hi');
+
+    listitem.innerHTML += value1
+    console.log(listitem)
+    listitem.innerHTML += value1
+    list.appendChild(listitem)
+    
+}
+
+var x = (a, b) => { // arrow 
+    return a+b;
+};
+
+x(1, 2);
+
+document.getElementById('aa');
+
+<input name="login"></input>
